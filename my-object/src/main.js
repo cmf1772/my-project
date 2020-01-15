5ba7d398512274bef4cbbb76b6aa50f329288fa2
chenmeifeng <18410105520@163.com>
2020-01-15 09:00:08 +0800

    全局axios

--- my-object/src/main.js
+++ my-object/src/main.js
@@ -3,8 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+
+Vue.prototype.axios = axios;
 
 Vue.config.productionTip = false
+
+
+axios.interceptors.response.use(
+  response => {
+    if (response.data.resultCode=="404") {
+        console.log("response.data.resultCode是404")
+        // 返回 错误代码-1 清除ticket信息并跳转到登录页面
+        //      cookie.del("ticket")
+        //      window.location.href='http://login.com'
+        return
+    }else{
+        return response;
+    }
+  },
+error => {
+  return Promise.reject(error.response)   // 返回接口返回的错误信息
+});
 
 /* eslint-disable no-new */
 new Vue({
